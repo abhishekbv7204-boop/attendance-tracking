1892d21c34122f0347478e2cd7c15d0a06ea4dbf
abhishekbv <abhishekbv7204@gmail.com>
2026-04-16 23:17:40 +0530

    Change database filename from attendance.data to attendance.db

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 import sqlite3
 def connect_db():
-    conn = sqlite3.connect("attendance.data", check_same_thread=False)
+    conn = sqlite3.connect("attendance.db", check_same_thread=False)
     conn.row_factory = sqlite3.Row
     return conn
 def ensure_column(cur, table, column, definition):
